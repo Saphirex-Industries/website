92147eb2cf475ede62443696857967ebd960cae5
0rangeFox <OrangeFox@outlook.pt>
2022-03-29 23:33:15 +0100

    ➕ Added parallax effect. (https://youtu.be/ADAmKlVs25w)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FunctionComponent, useEffect } from 'react';
+import * as CSS from 'csstype';
 import './App.css';
 
-function App() {
+const getRandomArbitrary = (min: number, max: number): number => Math.random() * (max - min) + min;
+
+const App: FunctionComponent = () => {
+
+  const onMouseMove = (event: MouseEvent) => document.documentElement.style.setProperty('--x', `${event.clientX}px`);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', onMouseMove);
+    return () => window.removeEventListener('mousemove', onMouseMove);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className='text'>
+        {
+          Array.from({ length: 10 }, (_, i) => (
+            <h2 key={i} style={{ '--i': getRandomArbitrary(-.1, 3) } as CSS.Properties}>
+              {Array.from({ length: 10 }, (_, ii) => <span key={ii}>Saphirex</span>)}
+            </h2>
+          ))
+        }
+      </div>
+    </section>
   );
 }
 
