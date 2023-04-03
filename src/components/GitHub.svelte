5b5704b93b5718ba7391266f437d917018bef4fd
0rangeFox <OrangeFox@outlook.pt>
2023-04-03 23:36:57 +0100

    🔧 Change framework from React to Svelte.

---
+++ src/components/GitHub.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+    import github_logo from '$lib/images/github-mark-white.svg';
+</script>
+
+<a href='https://github.com/Saphirex-Industries'>
+    <img src={github_logo}  alt='GitHub Logo' />
+</a>
+
+<style lang='scss'>
+    @import '$lib/styles/glow-effect.scss';
+
+    img {
+        @extend .glow;
+
+        width: 3vw;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang='ts'>
+	import '$lib/styles/styles.scss';
+
+	import logo from '$lib/images/logo-3000x2000.webp';
+	import logo_fallback from '$lib/images/logo-3000x2000.png';
+	import GitHub from '../components/GitHub.svelte';
+</script>
+
+<svelte:head>
+	<title>Saphirex</title>
+</svelte:head>
+
+<span class='root'>
+	<span class='container'>
+		<picture>
+			<source srcset={logo} type='image/webp' />
+			<img src={logo_fallback} alt='Saphirex Logo' />
+		</picture>
+	</span>
+</span>
+<span class='social_links'>
+	<GitHub />
+</span>
+
+<style lang='scss'>
+	@import '$lib/styles/glow-effect.scss';
+
+	.root {
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		background: linear-gradient(to right, #1c3f4c, #132a33, #0F2027);
+
+		.container {
+			width: 50%;
+
+			img {
+				@extend .glow;
+
+				width: 100%;
+			}
+		}
+	}
+
+	.social_links {
+		position: absolute;
+
+		bottom: 1.5vh;
+		right: 1.5vh;
+	}
+</style>
